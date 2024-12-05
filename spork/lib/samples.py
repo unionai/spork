623a9725fd734c24ae28fc3fba38d1bec60b86dc
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-12-05 15:58:31 -0800

    add a comment

--- spork/lib/samples.py
+++ spork/lib/samples.py
@@ -42,6 +42,9 @@ def fibonacci_dp(n):
 
 
 def fibonacci(n):
+    """
+    this is a comment
+    """
     sequence = [0, 1]
     for i in range(2, n):
         sequence.append(sequence[-1] + sequence[-2])
